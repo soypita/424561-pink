7c968577b0610fcc2f90d76bb4219ee67628cd2b
Dmitry <soyaburritos@gmail.com>
2017-10-15 15:08:31 +0300

    Инлайновые свг поменял на спрайты

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var rename = require("gulp-rename");
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
@@ -30,3 +31,35 @@ gulp.task("serve", ["style"], function() {
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
+
+var imagemin = require("gulp-imagemin");
+
+gulp.task("images", function(){
+  return gulp.src("img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+var webp = require("gulp-webp");
+
+gulp.task("webp", function() {
+  return gulp.src("img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("img"))
+});
+
+var svgstore = require("gulp-svgstore");
+
+gulp.task("sprite", function() {
+  return gulp.src("img/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("img"));
+});
+
