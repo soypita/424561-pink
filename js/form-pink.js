00793f6d2d6c54a81b3d116fd5428435e17147ac
Dmitry <soyaburritos@gmail.com>
2017-10-18 00:42:01 +0300

    Поправил js для шапки

--- js/form-pink.js
+++ js/form-pink.js
@@ -1,23 +1,14 @@
 var navMain = document.querySelector('.main-nav');
+var navToogleClose = document.querySelector('.main-nav__toggle');
 var navToggleCross = document.querySelector('.main-nav__toggle').querySelector('.main-nav__toggle-logo:nth-of-type(1)');
 var navToggleBurger = document.querySelector('.main-nav__toggle').querySelector('.main-nav__toggle-logo:nth-of-type(2)');
 
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
 navToggleCross.classList.remove('main-nav__toggle--open');
 navToggleBurger.classList.add('main-nav__toggle--open');
 
-navToggleCross.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navToggleBurger.classList.remove('main-nav__toggle--open');
-    navToggleCross.classList.add('main-nav__toggle--open');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navToggleBurger.classList.add('main-nav__toggle--open');
-    navToggleCross.classList.remove('main-nav__toggle--open');  }
-});
-
-navToggleBurger.addEventListener('click', function() {
+navToogleClose.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navToggleBurger.classList.remove('main-nav__toggle--open');
